5ef6f84efc4a7bdc7e30a8071db11da0349ed623
vonrepiks <ico_skipernov@abv.bg>
2017-10-17 14:58:09 +0300

    Add MONGOLAB_URI variable for mongolab db.

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     development: {
-        connectionString: "mongodb://localhost:27017/rent-a-car-db",
+        connectionString: process.env.MONGOLAB_URI || "mongodb://localhost:27017/rent-a-car-db",
         port: process.env.PORT || 3000
     },
     production: {
